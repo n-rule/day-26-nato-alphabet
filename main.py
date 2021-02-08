849054cbf1bf9fc5314052330f3efe052bb5ffa9
n-rule <rule@ukr.net>
2021-02-08 20:07:55 +0200

    1

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import pandas
 
 nato_alphabet = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
-phonetic_list = []
 
-while len(phonetic_list) < 1:
+
+def generate_phonetic():
     try:
         name = input('Enter your name : \n').upper()
         phonetic_list = [nato_alphabet_dict[letter] for letter in name]
-
     except KeyError:
         print('This is an ugly name')
+        generate_phonetic()
+    else:
+        print(phonetic_list)
 
-print(phonetic_list)
+generate_phonetic()
 
 # BOTH WORKS
 # phonetic_list = []
